a7f0fc3f94253e433d168239f41be0d57dacf70d
willywin99 <willywinsianturi@gmail.com>
2023-07-02 02:50:56 +0700

    add Luas Persegi Panjang

--- LuasPersegiPanjang.java
+++ LuasPersegiPanjang.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class LuasPersegiPanjang {
   public static void main(String[] args) {
-    
+    try (Scanner inputScanner = new Scanner(System.in)) {
+      System.out.print("Masukkan panjang : ");
+      double panjang = inputScanner.nextDouble();
+      System.out.print("Masukkan lebar : ");
+      double lebar = inputScanner.nextDouble();
+
+      System.out.println("Luas Persegi Panjang = " + panjang * lebar);
+    }
+
+    System.out.println();
   }
 }
